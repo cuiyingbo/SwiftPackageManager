ec3675e21be703f2c54b64dfe4b0d082405e5e5b
崔英博 <2313650391@qq.com>
2021-02-01 15:53:50 +0800

    alamofire依赖库使用示例

--- SPMLearner/SPMLearner/ViewController.swift
+++ SPMLearner/SPMLearner/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
-
+import Alamofire
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        testDependency()
     }
 
-
+    func testDependency() -> Void {
+        Alamofire.Session.default.request("https://bbs.hupu.com/40622370.html").response { (responseObj) in
+            guard let data = responseObj.data else { return }
+            let string2:String? =   String.init(data: data, encoding: .utf8)
+            guard let str = string2 else { return }
+            NSLog(str)
+        }
+    }
 }
 
