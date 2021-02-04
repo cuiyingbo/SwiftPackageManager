e35f4419e5b069d731caebf87565cb279dd10c8f
崔英博 <2313650391@qq.com>
2021-02-04 14:32:36 +0800

    添加RXSwift target

--- SomeFeature/Package.swift
+++ SomeFeature/Package.swift
@@ -16,6 +16,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.1"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.1.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,6 +25,7 @@ let package = Package(
             name: "SomeFeature",
             dependencies: ["Other",
                            "User",
+                           "RXSwift",
                            .product(name: "Alamofire", package: "Alamofire")],
             resources: [.copy("some.txt")]),
         .target(
@@ -32,8 +34,11 @@ let package = Package(
         .target(
             name: "User",
             dependencies: [.product(name: "Alamofire", package: "Alamofire")]),
+        .target(name: "RXSwift",
+                dependencies: [Target.Dependency.product(name: "Alamofire", package: "Alamofire")]),
         .testTarget(
             name: "SomeFeatureTests",
             dependencies: ["SomeFeature"]),
+        
     ]
 )
