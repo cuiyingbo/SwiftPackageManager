0d34022d80f427bf7d01929738e6acb241feb4be
崔英博 <2313650391@qq.com>
2021-03-24 14:36:52 +0800

    AnyObserver 的声明使用

--- SomeFeature/Sources/RXSwiftDemo/Observable& Observer/ObserverObj.swift
+++ SomeFeature/Sources/RXSwiftDemo/Observable& Observer/ObserverObj.swift
@@ -10,23 +10,29 @@ import RxSwift
 import RxCocoa
 import Alamofire
 public class ObserverObj:Any{
-//    private let _observer: AnyObserver<String>
+    private let _observer: AnyObserver<String>
     private var _message: Observable<String>!
     private let _disposeBag: DisposeBag = DisposeBag()
     private let _dispose = Disposables.create()//注意这里一定声明Disposable作为属性，否则 .disposed(by: _disposeBag)会提前执行，导致网络请求完成时，observer已经释放
     init() {
-     
+        _observer = AnyObserver{ event in
+            switch event{
+            case: .next(let string)
+                print("_observer:" + string)
+            default:
+                break
+            }
+        }
     }
 }
 
 extension ObserverObj{
     func test(){
+        
         _message = self.request(inputText: "abc")
-        _message.subscribe { (event) in
-            print(event)
-        }
-        .disposed(by: _disposeBag)
+        _message.bind(to: _observer).disposed(by: _disposeBag)
     }
+    
     func request(inputText: String) -> Observable<String> {
         print("请求网络了\(Thread.current)")
         return Observable<String>.create { [self] (ob) -> Disposable in
